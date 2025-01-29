3c2e998929c1380c7edd97fc23030334e1e9967e
shinymack <akashkawle2995@gmail.com>
2025-01-29 16:08:44 +0530

    project completed

---
+++ README.md
@@ -0,0 +1,3 @@
+## MERN Chatapp
+
+credits : https://youtu.be/ntKkVrQqBYY?si=qz02jMCQ80RT1jaH
---
+++ backend/src/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import path from "path";
+
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
+
+const __dirname = path.resolve();
+dotenv.config();
+app.use(cookieParser());
+app.use(express.json({limit : '2mb'}));
+app.use(express.urlencoded({ limit: '2mb', extended: true }));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+
+app.use("/api/auth", authRoutes );
+app.use("/api/messages", messageRoutes );
+
+
+
+const PORT = process.env.PORT;
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    
+    app.get("*" , (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend", "dist","index.html"));
+    })
+}
+
+server.listen(PORT, () => {
+    console.log("server is running on PORT: " + String(PORT));
+    connectDB();
+});
+
